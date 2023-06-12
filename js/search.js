175e247579338facef233b2c56a9838ffdd10291
DioonS <itzdiih@gmail.com>
2023-06-11 22:28:51 -0300

    correção de bugs nas imagens

--- js/search.js
+++ js/search.js
@@ -1,14 +1,3 @@
-/*function changeImage(imageName) {
-    // Seleciona todos os elementos com a classe "dropdown-image"
-    const images = document.querySelectorAll(".dropdown-image");
-    
-    // Itera sobre os elementos e altera a imagem correspondente
-    images.forEach(image => {
-        const src = `imagens/${imageName}.png`; // URL da imagem correspondente
-        image.src = src;
-    });
-}*/
-
 function myFunction() {
     var input, filter, cards, i, txtValue;
 
